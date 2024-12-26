5e67d24cca538ab129a7ee6eedbf0f3955ccd585
Dag7 <dag7@protonmail.com>
2024-12-26 23:46:09 +0000

    Working cognito login

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-      alias: [
-      {
-        find: './runtimeConfig',
-        replacement: './runtimeConfig.browser', // ensures browser compatible version of AWS JS SDK is used
-      },
-    ]
-  }
 })
